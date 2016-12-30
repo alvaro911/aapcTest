648bc38e685c6773b8c1f883e9cea2e242fa6f72
alvaro911 <algp232@hotmail.com>
2016-12-29 21:01:01 -0700

    node server added

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var path = require('path')
 var server = express();
+var port = process.env.PORT || 8080;
 var dist = path.join(__dirname, 'dist')
 server.use(express.static(dist))
-var serving = server.listen(8080);
-console.log('serving at', serving);
+var serving = server.listen(port);
+console.log('serving at', port);
